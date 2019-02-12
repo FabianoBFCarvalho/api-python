87a1933be0a93d7c4fed480a1ca7adfecf336e28
FabianoBFCarvalho <ffcarvalho426@gmail.com>
2019-02-11 23:39:22 -0200

    Commit

--- storage/bucket.py
+++ storage/bucket.py
@@ -1,5 +1,8 @@
 # from beaker.ext import google
 from google.cloud import storage
+# import os
+# import cloudstorage
+# from google.appengine.api import app_identity
 
 
 class Bucket:
@@ -20,10 +23,39 @@ class Bucket:
             print("Sorry, that bucket does not exist! creating...")
             cls.create_bucket(bucket_name)
 
+    # def get(self):
+    #     bucket_name = os.environ.get(
+    #         'BUCKET_NAME', app_identity.get_default_gcs_bucket_name())
+    #
+    #     self.response.headers['Content-Type'] = 'text/plain'
+    #     self.response.write(
+    #         'Demo GCS Application running from Version: {}\n'.format(
+    #             os.environ['CURRENT_VERSION_ID']))
+    #     self.response.write('Using bucket name: {}\n\n'.format(bucket_name))
+    #
+    # def create_file(self, filename):
+    #     """Create a file."""
+    #
+    #     self.response.write('Creating file {}\n'.format(filename))
+    #
+    #     # The retry_params specified in the open call will override the default
+    #     # retry params for this particular file handle.
+    #     write_retry_params = cloudstorage.RetryParams(backoff_factor=1.1)
+    #     with cloudstorage.open(
+    #             filename, 'w', content_type='text/plain', options={
+    #                 'x-goog-meta-foo': 'foo', 'x-goog-meta-bar': 'bar'},
+    #             retry_params=write_retry_params) as cloudstorage_file:
+    #         cloudstorage_file.write('abcde\n')
+    #         cloudstorage_file.write('f' * 1024 * 4 + '\n')
+    #     self.tmp_filenames_to_clean_up.append(filename)
+
     @classmethod
     def upload_blob(cls, bucket_name, source_file_name, destination_blob_name):
         """Uploads a file to the bucket."""
-        cls.create_bucket(bucket_name)
+
+        # bucket_name = os.environ.get(
+        #     bucket_name, app_identity.get_default_gcs_bucket_name())
+        #
         storage_client = storage.Client()
         bucket = storage_client.get_bucket(bucket_name)
         blob = bucket.blob(destination_blob_name)
